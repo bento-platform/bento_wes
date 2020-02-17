e284dc264cc0e5e3c5dbc8bf8acd7af0ccce721c
David Lougheed <david.lougheed@gmail.com>
2020-02-17 15:59:35 -0500

    Use chord_lib-provided flask error wrapper functions
    Move SERVICE_TYPE out of application config

--- chord_wes/app.py
+++ chord_wes/app.py
@@ -1,7 +1,5 @@
 import chord_wes
 import os
-import sys
-import traceback
 
 from chord_lib.responses.flask_errors import *
 from flask import Flask, json, jsonify
@@ -14,6 +12,9 @@ from .events import *
 from .runs import bp_runs
 
 
+SERVICE_NAME = "CHORD WES"
+
+
 application = Flask(__name__)
 application.config.from_mapping(
     CHORD_SERVICES=os.environ.get("CHORD_SERVICES", "chord_services.json"),
@@ -23,32 +24,17 @@ application.config.from_mapping(
     DATABASE=os.environ.get("DATABASE", "chord_wes.db"),
     SERVICE_ID=SERVICE_ID,
     SERVICE_TEMP=os.environ.get("SERVICE_TEMP", "tmp"),
-    SERVICE_TYPE=SERVICE_TYPE,
     SERVICE_URL_BASE_PATH=os.environ.get("SERVICE_URL_BASE_PATH", "/"),
     WOM_TOOL_LOCATION=os.environ.get("WOM_TOOL_LOCATION", "womtool.jar")
 )
 
 application.register_blueprint(bp_runs)
 
-
-# TODO: Figure out common pattern and move to chord_lib
-
-def _wrap_tb(func):  # pragma: no cover
-    # TODO: pass exception?
-    def handle_error(_e):
-        print("[CHORD WES] Encountered error:", file=sys.stderr)
-        traceback.print_exc()
-        return func()
-    return handle_error
-
-
-def _wrap(func):  # pragma: no cover
-    return lambda _e: func()
-
-
-application.register_error_handler(Exception, _wrap_tb(flask_internal_server_error))  # Generic catch-all
-application.register_error_handler(BadRequest, _wrap(flask_bad_request_error))
-application.register_error_handler(NotFound, _wrap(flask_not_found_error))
+# Generic catch-all
+application.register_error_handler(Exception, flask_error_wrap_with_traceback(flask_internal_server_error,
+                                                                              service_name=SERVICE_NAME))
+application.register_error_handler(BadRequest, flask_error_wrap(flask_bad_request_error))
+application.register_error_handler(NotFound, flask_error_wrap(flask_not_found_error))
 
 
 def configure_celery(app):
@@ -86,7 +72,7 @@ def service_info():
     return jsonify({
         "id": application.config["SERVICE_ID"],
         "name": "CHORD WES",  # TODO: Should be globally unique?
-        "type": application.config["SERVICE_TYPE"],
+        "type": SERVICE_TYPE,
         "description": "Workflow execution service for a CHORD application.",
         "organization": {
             "name": "C3G",
