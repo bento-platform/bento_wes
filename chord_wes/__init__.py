49bdd12e4ad791631b79d674b019257cc27a8811
David Lougheed <david.lougheed@gmail.com>
2020-03-25 17:45:11 -0400

    Fix bad path to cfg file

--- chord_wes/__init__.py
+++ chord_wes/__init__.py
@@ -4,7 +4,7 @@ import os
 __all__ = ["name", "__version__"]
 
 config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), "chord_wes", "package.cfg"))
+config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), "package.cfg"))
 
 name = config["package"]["name"]
 __version__ = config["package"]["version"]
