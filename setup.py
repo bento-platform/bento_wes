fb2eb15ac0922c3651d2a2c1a42f4e8e0e9ba715
David Lougheed <david.lougheed@gmail.com>
2020-06-15 12:56:39 -0400

    Switch to dev bento_lib for now

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     python_requires=">=3.6",
     install_requires=[
         "celery[redis]==4.4.2",
-        "bento_lib[flask]==0.10.0",
+        "bento_lib[flask] @ git+https://github.com/bento-platform/bento_lib.git@develop",
         "Flask>=1.1.2,<2.0",
         "requests>=2.23,<3.0",
         "requests-unixsocket>=0.2.0,<0.3.0",
