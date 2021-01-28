07ac55c75a625d62dbb833bd233725a2927cecf4
David Lougheed <david.lougheed@gmail.com>
2021-01-28 14:21:40 -0500

    Remove unused Config class items

--- bento_wes/config.py
+++ bento_wes/config.py
@@ -16,8 +16,6 @@ NGINX_INTERNAL_SOCKET = quote(os.environ.get("NGINX_INTERNAL_SOCKET", "/chord/tm
 class Config:
     CHORD_SERVICES = os.environ.get("CHORD_SERVICES", "chord_services.json")
     CHORD_URL = os.environ.get("CHORD_URL", "http://127.0.0.1:5000/")
-    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://")
-    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://")
     DATABASE = os.environ.get("DATABASE", "bento_wes.db")
     SERVICE_ID = SERVICE_ID
     SERVICE_TEMP = os.environ.get("SERVICE_TEMP", "tmp")
