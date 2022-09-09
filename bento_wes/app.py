e9e7e0b1b532f2a13fb6e4c9076d995e88f611b7
Paul Pillot <paul.pillot@mcgill.ca>
2022-09-09 06:42:35 -0400

    consistent debugger port with vscode
    Previous default port was conflicting with Katsu's

--- bento_wes/app.py
+++ bento_wes/app.py
@@ -71,7 +71,7 @@ def service_info():
 # # debugger section
 if application.config["BENTO_DEBUG"]:
     import debugpy
-    DEBUGGER_PORT = int(os.environ.get('DEBUGGER_PORT', 5678))
+    DEBUGGER_PORT = int(os.environ.get('DEBUGGER_PORT', 5680))
     debugpy.listen(("0.0.0.0", DEBUGGER_PORT))
     print('Attached')
 # # end debugger section
