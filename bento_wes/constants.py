1ae10de5755d93c748c55e2376ba9900ac69c269
David Lougheed <david.lougheed@gmail.com>
2022-12-06 14:14:06 -0500

    chore!: fix service type to conform to service info spec

--- bento_wes/constants.py
+++ bento_wes/constants.py
@@ -10,6 +10,10 @@ __all__ = [
 ]
 
 SERVICE_ARTIFACT = "wes"
-SERVICE_TYPE = f"ca.c3g.bento:{SERVICE_ARTIFACT}:{bento_wes.__version__}"
-SERVICE_ID = os.environ.get("SERVICE_ID", SERVICE_TYPE)
+SERVICE_TYPE = {
+    "group": "ca.c3g.bento",
+    "artifact": SERVICE_ARTIFACT,
+    "version": bento_wes.__version__,
+}
+SERVICE_ID = os.environ.get("SERVICE_ID", ":".join(SERVICE_TYPE.values()))
 SERVICE_NAME = "Bento WES"
