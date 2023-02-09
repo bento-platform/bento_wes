755b8a219fd7e32b4abfbe205b135f709fe43bfc
David Lougheed <david.lougheed@gmail.com>
2023-02-09 16:39:05 -0500

    chore: update entrypoint logs, set gitconfig in dev mode

--- entrypoint.dev.bash
+++ entrypoint.dev.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export FLASK_APP=bento_wes.app:application
+# Set .gitconfig for development
+/set_gitconfig.bash
+
+export FLASK_APP="bento_wes.app:application"
 
 if [ -z "${INTERNAL_PORT}" ]; then
   # Set default internal port to 5000
@@ -12,13 +15,13 @@ python -m poetry install
 
 # Clean up after any crashed previous container runs
 job_store_path="${SERVICE_TEMP:-tmp}/toil_job_store"
-if [ -d "${job_store_path}" ]; then
-  echo "[ENTRYPOINT] Cleaning Toil job store"
+if [[ -d "${job_store_path}" ]]; then
+  echo "[bento_wes] [entrypoint] Cleaning Toil job store"
   toil clean "file:${SERVICE_TEMP:-tmp}/toil_job_store"
 fi
 
 # Start Celery worker with log level dependent on BENTO_DEBUG
-echo "[ENTRYPOINT] Starting celery worker"
+echo "[bento_wes] [entrypoint] Starting celery worker"
 celery_log_level="INFO"
 if [[
   "${BENTO_DEBUG}" == "true" ||
@@ -33,7 +36,7 @@ fi
 celery --app bento_wes.app worker --loglevel="${celery_log_level}" &
 
 # Start API server
-echo "[ENTRYPOINT] Starting Flask server"
+echo "[bento_wes] [entrypoint] Starting Flask server"
 python -m debugpy --listen 0.0.0.0:5678 -m flask run \
   --host 0.0.0.0 \
   --port "${INTERNAL_PORT}"
