168becf7e397c9bb425d5484599cc883addc0c6a
David Lougheed <david.lougheed@gmail.com>
2023-03-20 22:43:28 -0400

    fix: configurable debugger port

--- run.dev.bash
+++ run.dev.bash
@@ -11,11 +11,6 @@ pip install -e .
 
 export FLASK_APP="bento_wes.app:application"
 
-if [[ -z "${INTERNAL_PORT}" ]]; then
-  # Set default internal port to 5000
-  export INTERNAL_PORT=5000
-fi
-
 # Create temporary directory if needed
 mkdir -p /wes/tmp
 
@@ -41,8 +36,14 @@ if [[
 fi
 celery --app bento_wes.app worker --loglevel="${celery_log_level}" &
 
+# Set default internal port to 5000
+: "${INTERNAL_PORT:=5000}"
+
+# Set internal debug port, falling back to debugpy default
+: "${BENTO_WES_DEBUGGER_INTERNAL_PORT:=5680}"
+
 # Start API server
 echo "[bento_wes] [entrypoint] Starting Flask server"
-python -m debugpy --listen 0.0.0.0:5678 -m flask run \
+python -m debugpy --listen 0.0.0.0:${BENTO_WES_DEBUGGER_INTERNAL_PORT} -m flask run \
   --host 0.0.0.0 \
   --port "${INTERNAL_PORT}"
