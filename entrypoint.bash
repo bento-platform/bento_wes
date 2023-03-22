8aefef9f045ba4f307648ac69f2eb27d4cf6b5f1
David Lougheed <david.lougheed@gmail.com>
2023-03-22 16:56:48 -0400

    chore: rm dead entrypoint code to fix env perms

--- entrypoint.bash
+++ entrypoint.bash
@@ -8,9 +8,6 @@ source /create_service_user.bash
 # Fix permissions on /wes
 chown -R bento_user:bento_user /wes
 chmod -R o-rwx /wes/tmp  # Remove all access from others for /wes/tmp
-if [[ -d /env ]]; then
-  chown -R bento_user:bento_user /env
-fi
 
 # Drop into bento_user from root and execute the CMD specified for the image
 exec gosu bento_user "$@"
