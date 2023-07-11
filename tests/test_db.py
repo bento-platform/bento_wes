5792f1e5a110d784ff58a5efe4502a665256e0bd
David Lougheed <david.lougheed@gmail.com>
2023-07-11 13:40:16 -0400

    test: fix db import

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,11 +1,11 @@
 import sqlite3
 from flask import g
 
-import bento_wes.db as db
-
 
 # noinspection PyUnusedLocal,PyProtectedMember
 def test_db(client):
+    from bento_wes import db
+
     # Force db init by yielding client
 
     assert isinstance(db.get_db(), sqlite3.Connection)
@@ -18,4 +18,4 @@ def test_db(client):
     assert db._strip_first_slash("test/app/") == "test/app/"
 
     db.close_db(None)
-    assert g.get(db, None) is None
+    assert g.get("db", None) is None
