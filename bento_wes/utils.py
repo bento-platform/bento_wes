29df00fcbaaac46026ecf96d57c480fbee8700de
David Lougheed <david.lougheed@gmail.com>
2024-04-11 13:19:29 -0400

    chore: remove deprecated datetime uses + update lockfile

--- bento_wes/utils.py
+++ bento_wes/utils.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 __all__ = ["iso_now"]
 
 
 def iso_now() -> str:
-    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")  # ISO date format
+    return datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")  # ISO date format
