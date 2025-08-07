b6bcec9394e88eef83a42a9ecc9b723732a7fd29
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2025-08-07 19:51:51 +0000

    removed old tests

--- tests/test_service_info.py
+++
@@ -1,9 +0,0 @@
-import bento_lib
-from jsonschema import validate
-
-
-def test_service_info(client):
-    rv = client.get("/service-info")
-    data = rv.get_json()
-
-    validate(data, bento_lib.schemas.ga4gh.SERVICE_INFO_SCHEMA)
