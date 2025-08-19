b9f63c25a47065495dd456e01924441698f309e6
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2025-08-19 16:57:19 +0000

    removed flask authz

--- bento_wes/authz.py
+++ bento_wes/authz.py
@@ -1,19 +1,7 @@
-from bento_lib.auth.middleware.flask import FlaskAuthMiddleware
 from bento_lib.auth.middleware.fastapi import FastApiAuthMiddleware
 from .config import config
 from .logger import logger
 
-__all__ = [
-    "authz_middleware",
-    "authz_middleware_flask"
-]
-
 authz_middleware = FastApiAuthMiddleware.build_from_fastapi_pydantic_config(
     config, logger
-)
-
-authz_middleware_flask = FlaskAuthMiddleware(
-    config.authz_url,
-    debug_mode=config.bento_debug,
-    enabled=config.authz_url,
 )
