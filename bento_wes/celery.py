e1d0fa25195ba44123789c1dab5c5af4ead43c54
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2025-09-04 16:27:08 +0000

    made celeryconfig use pydantic v2 config

--- bento_wes/celery.py
+++ bento_wes/celery.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 from celery import Celery
 
 
@@ -6,9 +7,10 @@ class CeleryConfig(BaseSettings):
     celery_result_backend: str = "redis://"
     celery_broker_url: str = "redis://"
 
-    class Config:
-        env_prefix = "CELERY_"
-        case_sensitive = False
+    model_config = ConfigDict(
+        env_prefix="CELERY_",
+        case_sensitive=False
+    )
 
 
 config = CeleryConfig()
