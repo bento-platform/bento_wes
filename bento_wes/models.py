29e4088e85488df588f607bc017fa87fd638bf87
Sanjeev Lakhwani <lakhwani.sanjeev691@gmail.com>
2025-10-29 18:19:57 +0000

    corrected return type

--- bento_wes/models.py
+++ bento_wes/models.py
@@ -1,7 +1,7 @@
 from bento_lib.workflows.models import WorkflowDefinition, WorkflowProjectDatasetInput
 from bento_lib.workflows.utils import namespaced_input
 from bento_lib.auth.resources import RESOURCE_EVERYTHING, build_resource
-from bento_lib.auth.permissions import P_ANALYZE_DATA, P_INGEST_DATA, P_EXPORT_DATA
+from bento_lib.auth.permissions import P_ANALYZE_DATA, P_INGEST_DATA, P_EXPORT_DATA, Permission
 
 from datetime import datetime
 from pydantic import BaseModel, ConfigDict, AnyUrl, Json
@@ -65,7 +65,7 @@ class RunRequest(BaseModel):
 
         return resource
 
-    def get_workflow_permission(self) -> str:
+    def get_workflow_permission(self) -> Permission:
         match self.tags.workflow_metadata.type:
             case "ingestion":
                 return P_INGEST_DATA
